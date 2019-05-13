cf1fb180941364f253de8aea9e1c811abe8b89db
krishna <krishna.moorthy6@wipro.com>
2019-05-13 20:06:22 +0530

    Ignore additional fields in FM and PM messages
    Change-Id: I5de3f6d6c5f0bbf0e7d4f250b84ab51e5482525b
    Issue-ID: DCAEGEN2-1497
    Signed-off-by: krishna <krishna.moorthy6@wipro.com>

--- src/main/java/org/onap/dcaegen2/services/sonhms/FaultFields.java
+++ src/main/java/org/onap/dcaegen2/services/sonhms/FaultFields.java
@@ -21,6 +21,9 @@
 
 package org.onap.dcaegen2.services.sonhms;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FaultFields {
 
     @Override
