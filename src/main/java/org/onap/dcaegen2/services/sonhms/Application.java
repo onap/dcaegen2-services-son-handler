cee68e2e64d35019b7b66570062bd6e8e7981dc5
krishna <krishna.moorthy6@wipro.com>
2020-02-08 14:11:41 +0530

    Update microservice to run as standalone MS
    Issue-ID: DCAEGEN2-2066
    Signed-off-by: krishna <krishna.moorthy6@wipro.com>
    Change-Id: I3305d6df039ba2c3a9283bf6cd4c50ca073d12c8

--- src/main/java/org/onap/dcaegen2/services/sonhms/Application.java
+++ src/main/java/org/onap/dcaegen2/services/sonhms/Application.java
@@ -2,26 +2,34 @@
  *  ============LICENSE_START=======================================================
  *  son-handler
  *  ================================================================================
- *   Copyright (C) 2019 Wipro Limited.
+ *   Copyright (C) 2019-2020 Wipro Limited.
  *   ==============================================================================
  *     Licensed under the Apache License, Version 2.0 (the "License");
  *     you may not use this file except in compliance with the License.
  *     You may obtain a copy of the License at
- *  
+ *
  *          http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *     Unless required by applicable law or agreed to in writing, software
  *     distributed under the License is distributed on an "AS IS" BASIS,
  *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *     See the License for the specific language governing permissions and
  *     limitations under the License.
  *     ============LICENSE_END=========================================================
- *  
+ *
  *******************************************************************************/
 
 package org.onap.dcaegen2.services.sonhms;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.reflect.Type;
 import java.time.Duration;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -33,6 +41,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
 public class Application {
 
@@ -42,6 +51,40 @@ public class Application {
      * Main method where the pci context is initially set.
      */
     public static void main(String[] args) {
+        getConfig();
+        log.info("Starting spring boot application");
+        SpringApplication.run(Application.class);
+
+    }
+
+    private static void getConfig() {
+
+        Boolean standalone = Boolean.parseBoolean(System.getenv("STANDALONE"));
+
+        if (standalone) {
+            log.info("Running in standalone mode");
+
+            String configFile = System.getenv("CONFIG_FILE");
+            String configAllJson = readFromFile(configFile);
+
+            JsonObject configAll = new Gson().fromJson(configAllJson, JsonObject.class);
+
+            JsonObject config = configAll.getAsJsonObject("config");
+
+            Configuration.getInstance().updateConfigurationFromJsonObject(config);
+
+            ConfigPolicy configPolicy = ConfigPolicy.getInstance();
+            Type mapType = new TypeToken<Map<String, Object>>() {
+            }.getType();
+            if (configAll.getAsJsonObject("policies") != null) {
+                JsonObject policyJson = configAll.getAsJsonObject("policies").getAsJsonArray("items").get(0)
+                        .getAsJsonObject().getAsJsonObject("config");
+                Map<String, Object> policy = new Gson().fromJson(policyJson, mapType);
+                configPolicy.setConfig(policy);
+                log.info("Config policy {}", configPolicy);
+            }
+            return;
+        }
 
         ConfigFetchFromCbs configFetchFromCbs = new ConfigFetchFromCbs(Duration.ofSeconds(60));
         Thread configFetchThread = new Thread(configFetchFromCbs);
@@ -52,9 +95,6 @@ public class Application {
             log.debug("InterruptedException : {}", e);
         }
         log.info("after 10s sleep");
-        log.info("Starting spring boot application");
-        SpringApplication.run(Application.class);
-
     }
 
     /**
@@ -68,6 +108,21 @@ public class Application {
 
         return DataSourceBuilder.create().url(url).username(configuration.getPgUsername())
                 .password(configuration.getPgPassword()).build();
+    }
+
+    private static String readFromFile(String file) {
+        String content = "";
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            content = bufferedReader.readLine();
+            String temp;
+            while ((temp = bufferedReader.readLine()) != null) {
+                content = content.concat(temp);
+            }
+            content = content.trim();
+        } catch (Exception e) {
+            content = null;
+        }
+        return content;
     }
 
 }
