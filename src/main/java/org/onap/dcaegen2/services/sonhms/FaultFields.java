d9b2a6fef81bf657eac6224dba75254db49c926b
Yashwanth <gandhapu.yashwanth@wipro.com>
2021-08-10 04:42:33 +0000

    Update FM message spec to VES 7.2
    Issue-ID: DCAEGEN2-2884
    
    Signed-off-by: Yashwanth <gandhapu.yashwanth@wipro.com>
    Change-Id: I8f609af56604fae86b8a75aaadce14c78ba8ffb8

--- src/main/java/org/onap/dcaegen2/services/sonhms/FaultFields.java
+++ src/main/java/org/onap/dcaegen2/services/sonhms/FaultFields.java
@@ -2,7 +2,7 @@
  *  ============LICENSE_START=======================================================
  *  son-handler
  *  ================================================================================
- *   Copyright (C) 2019 Wipro Limited.
+ *   Copyright (C) 2019-2021 Wipro Limited.
  *   ==============================================================================
  *     Licensed under the Apache License, Version 2.0 (the "License");
  *     you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ public class FaultFields {
     @Override
     public String toString() {
         return "FaultFields [faultFieldsVersion=" + faultFieldsVersion + ", alarmCondition=" + alarmCondition
-                + ", eventSourceType=" + eventSourceType + ", specificProblem=" + specificProblem + ", eventSeverity="
+                + ", eventSourceType=" + eventSourceType + ", eventCategory=" + eventCategory  + ", specificProblem=" + specificProblem + ", eventSeverity="
                 + eventSeverity + ", vfStatus=" + vfStatus + ", alarmAdditionalInformation="
                 + alarmAdditionalInformation + "]";
     }
@@ -37,6 +37,7 @@ public class FaultFields {
     private double faultFieldsVersion;
     private String alarmCondition;
     private String eventSourceType;
+    private String eventCategory;
     private String specificProblem;
     private String eventSeverity;
     private String vfStatus;
@@ -74,6 +75,14 @@ public class FaultFields {
         this.eventSourceType = eventSourceType;
     }
 
+    public String getEventCategory() {
+        return eventCategory;
+    }
+
+    public void setEventCategory(String eventCategory) {
+        this.eventCategory = eventCategory;
+    }
+    
     public String getSpecificProblem() {
         return specificProblem;
     }
