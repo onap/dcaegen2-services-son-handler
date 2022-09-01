ba0f52323d52beccf6e93e49f12953d85cc671c2
malar <malarvizhi.44@wipro.com>
2022-09-01 11:00:01 +0000

    Modify payload for ANR closedloop to align with A1 based action
    Issue-ID: DCAEGEN2-3148
    Signed-off-by: Malarvizhi Paramasivam <malarvizhi.44@wipro.com>
    Change-Id: I41264a68b3dd40306c59a1c39fc1fd47213a3b13

--- src/main/java/org/onap/dcaegen2/services/sonhms/model/PolicyNotification.java
+++ src/main/java/org/onap/dcaegen2/services/sonhms/model/PolicyNotification.java
@@ -2,14 +2,14 @@
  *  ============LICENSE_START=======================================================
  *  son-handler
  *  ================================================================================
- *   Copyright (C) 2019-2020 Wipro Limited.
+ *   Copyright (C) 2019-2022 Wipro Limited.
  *   ==============================================================================
  *     Licensed under the Apache License, Version 2.0 (the "License");
  *     you may not use this file except in compliance with the License.
  *     You may obtain a copy of the License at
- *  
+ *
  *          http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *     Unless required by applicable law or agreed to in writing, software
  *     distributed under the License is distributed on an "AS IS" BASIS,
  *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -65,7 +65,7 @@ public class PolicyNotification {
      * Constructor.
      */
     public PolicyNotification(String closedLoopControlName, String requestId, Long alarmStartTime, String pnfName,
-            String action) {
+            String action, String policyName) {
         this.closedLoopControlName = closedLoopControlName;
         this.requestId = requestId;
         this.closedLoopEventClient = "microservice.PCI";
@@ -80,7 +80,7 @@ public class PolicyNotification {
         aai.put("generic-vnf.is-closed-loop-disabled", "false");
         aai.put("generic-vnf.prov-status", "ACTIVE");
         aai.put("pnf.pnf-name", pnfName);
-        this.policyName = "PCI";
+        this.policyName = policyName;
         this.policyVersion = "1.0.2";
     }
 
